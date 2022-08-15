fd75d4744bc00cc1258b7b58ce6117ffff15d15c
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-08-15 15:52:49 -0600

    Vue Intro

--- src/components/ConditionalRenderingExercise.vue
+++ src/components/ConditionalRenderingExercise.vue
@@ -10,7 +10,7 @@
             screen.
           </p>
           <!-- add code to element below -->
-          <div class="player-card text-center">
+          <div v-if="!state.player.showPlayer" class="player-card text-center" >
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -67,23 +67,23 @@
             to add another condition.
           </p>
           <!-- v-if comparing grade and 90 -->
-          <div>
-            <p>The grade is 'A'.</p>
+          <div v-if="state.player.grade >= 90">
+            <p >The grade is 'A'.</p>
           </div>
           <!-- v-else-if comparing grade and 80 -->
-          <div>
-            <p>The grade is 'B'.</p>
+          <div v-else-if="state.player.grade >= 80">
+            <p >The grade is 'B'.</p>
           </div>
           <!-- v-else-if comparing grade and 70 -->
-          <div>
+          <div v-else-if="state.player.grade >= 70">
             <p>The grade is 'C'.</p>
           </div>
           <!-- v-else-if comparing grade and 60 -->
-          <div>
+          <div v-else-if="state.player.grade >= 60">
             <p>The grade is 'D'.</p>
           </div>
           <!-- v-else to display if all the others fail -->
-          <div>
+          <div v-else>
             <p>The grade is 'F'.</p>
           </div>
         </div>
@@ -93,7 +93,7 @@
             Change the v-if directive to a v-show on the "player-card" element
             below.
           </p>
-          <div class="player-card text-center area" v-show="state.player.id">
+          <div class="player-card text-center area" v-show="!state.player.id">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -132,10 +132,12 @@ export default {
       //add a property to toggle the player-card here.
       //add a property to set the number value here.
       player: {
+        showPlayer: false,
         photo: "https://robohash.org/Mick",
         name: "Mick",
         position: "WR",
         number: 4,
+        grade: 50,
       },
     });
     return {
